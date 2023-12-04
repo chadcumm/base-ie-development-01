34852d783209054c11558caef12058d925580ab4
chad cummings <algeron@gmail.com>
2023-12-04 15:45:49 -0600

    updates from initial release

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ie-mpage'),
+      dir: require('path').join(__dirname, './coverage/base-ie-development-01'),
       subdir: '.',
       reporters: [
         { type: 'html' },
