e6329e0f7d1f0112f31e44dc9a0a66803e3109d3
chadcumm <algeron@gmail.com>
2023-12-11 17:24:29 -0500

    corrected initial servives load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,23 +16,26 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
-    // Grab any parameters in the URL (Used in Cerner Components)
-    this.activatedRoute.queryParams.subscribe(params => {
-      this.mPage.personId = params.personId ? parseInt(params.personId) : this.mPage.personId;
-      this.mPage.encntrId = params.encounterId ? parseInt(params.encounterId) : this.mPage.encntrId;
-      this.mPage.prsnlId = params.userId ? parseInt(params.userId) : this.mPage.prsnlId;
-    });
+    // // Grab any parameters in the URL (Used in Cerner Components)
+    // this.activatedRoute.queryParams.subscribe(params => {
+    //   this.mPage.personId = params.personId ? parseInt(params.personId) : this.mPage.personId;
+    //   this.mPage.encntrId = params.encounterId ? parseInt(params.encounterId) : this.mPage.encntrId;
+    //   this.mPage.prsnlId = params.userId ? parseInt(params.userId) : this.mPage.prsnlId;
+    // });
 
     // Perform MPage Initialization
     setTimeout((e: any) => {
       this.mPage.setMaxInstances(2, true, 'CHART');
       this.mPage.executeCCL ({
-
-        patientSource: [{personId: 0, encntrId: 0}],
-        person: {},
-        encounter: {},
-        problem: {}
-
+        payload: {
+          patientSource: [{personId: 0, encntrId: 0}],
+          person: { 
+              patient: true,
+              aliases: true,
+          },
+          encounter: { },
+          problem: { }
+        }
       })
       // Add your initialization code here - do not place outside setTimeout function
     }, 0);
